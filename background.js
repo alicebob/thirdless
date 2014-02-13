557ce78e497a0a1f3a4dd5494fd9cc6192beea35
Harmen <harm@dds.nl>
2014-02-13 21:02:47 +0100

    use 'main_frame' to find user trigger request.

--- background.js
+++ background.js
@@ -9,37 +9,58 @@ var strippedReceivedHeaders = {
     // More?
 };
 
-var goodHosts = {};
+var goodHosts = {
+    // maps.google
+    "mts0.google.com": true,
+    "mts1.google.com": true,
+};
 
+function getHostname(url) {
+    if (match = url.match(/^https?:\/\/([^\/]+)\//)) {
+        return match[1];
+    }
+}
+
+/*
 function onNavigate(details) {
-    // console.log("onNavigate", details.url);
+    console.log("onNavigate", details.url, details.type);
     var match;
     // frameId 0 are top level. This still leaves popups with commercials,
     // though.
     if (details.frameId != 0) return;
-    if (match = details.url.match(/^https?:\/\/([^\/]+)\//)) {
-        console.log("Accepting ", match[1]);
-        goodHosts[match[1]] = true;
+    var hostname;
+    if (hostname = getHostname(details.url)) {
+        console.log("Accepting ", hostname);
+        goodHosts[hostname] = true;
     }
 }
+*/
 
 function onBeforeHeadersSend(details) {
+    // console.log("onBeforeHeadersSend", details.url, details.type);
     // console.log("sending", details.url, details.requestHeaders);
     var requestHost = '';
-    var match = details.url.match(/^https?:\/\/([^\/]+)\//);
-    if (! match) {
+    var hostname = getHostname(details.url);
+    if (! hostname) {
         // Weird.
         console.log("No host in ", details.url);
         return;
     }
-    requestHost = match[1];
-    if (requestHost in goodHosts) {
+
+    if (details.type === "main_frame") {
+        // Requests is top level. We accept this host from now on.
+        console.log("Accepting due to main_frame: ", hostname);
+        goodHosts[hostname] = true;
+        return;
+    }
+    if (hostname in goodHosts) {
         // Known host. Allow all headers.
         // console.log("Allow as-is", details.url);
         return;
     }
     // 3rd party request. Strip some headers.
     var changed = false;
+    console.log("Stripping headers to ", details.url);
     for (var i = 0; i < details.requestHeaders.length; ) {
         if (details.requestHeaders[i].name in strippedSendHeaders) {
             // console.log("Stripping send header ", details.requestHeaders[i], details.url);
@@ -56,22 +77,34 @@ function onBeforeHeadersSend(details) {
 }
 
 function onBeforeHeadersResponse(details) {
+    // console.log("onBeforeHeadersResponse", details.url);
     // console.log("receiving", details.url, details.responseHeaders);
+    var to, host;
 
-    var host = '';
-    var match = details.url.match(/^https?:\/\/([^\/]+)\//);
-    if (! match) {
+    host = getHostname(details.url);
+    if (! host) {
         // Weird.
         // console.log("No host in ", details.url);
         return;
     }
-    host = match[1];
     if (host in goodHosts) {
         // Known host. Allow all headers.
         // console.log("Allow received as-is", details.url);
+        /*
+        for (var i = 0; i < details.responseHeaders.length; i++) {
+            if (details.responseHeaders[i].name === "Location") {
+                if (to = getHostname(details.responseHeaders[i].value)) {
+                    console.log("Is a redirect to ", to);
+                    goodHosts[to] = true;
+                }
+                break;
+            }
+        }
+        */
         return;
     }
     // 3rd party request. Strip some headers.
+    console.log("Stripping received headers from ", details.url);
     var changed = false;
     for (var i = 0; i < details.responseHeaders.length; ) {
         if (details.responseHeaders[i].name in strippedReceivedHeaders) {
@@ -90,8 +123,8 @@ function onBeforeHeadersResponse(details) {
     return {responseHeaders: details.responseHeaders};
 }
 
-// TODO: maybe via onHistoryStateUpdated?
-chrome.webNavigation.onBeforeNavigate.addListener(onNavigate);
+// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {console.log("onHistoryStateUpdated", details.url);});
+// chrome.webNavigation.onBeforeNavigate.addListener(onNavigate);
 chrome.webRequest.onBeforeSendHeaders.addListener(
     onBeforeHeadersSend,
     {
