585ad0a691c735ae08afdabd2d1707ae5e6a7799
Harmen <harm@dds.nl>
2014-02-13 23:11:23 +0100

    Don't whitelist sites visited in redirects only (That's how you get logged in on youtube)

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 Experimental Chrome extension.
 
-removes all 'identifing' headers from HTTP requests to requests other than the
-site you're looking at. Removes cookies, user-agent, referer.
-Requests are still made, so you will still be tracked via your IPnumber, but we make
-it a little harded to expose useful information, while keeping most legit
-functionality in tact.
+Removes all 'identifing' headers from HTTP requests to requests other than the
+site you're looking at, or have looked at in the tab. Removes cookies, user-agent, referer and some other leaky headers.
+
+Requests to third party sites are still made, so you will still be tracked via
+your IPnumber, but we make it a little harded to expose useful information,
+while keeping most legit functionality in tact.
+
+Cookies set in redirects chains to unknown sites don't get set.
+If you log in on accounts.google.com for gmail it'll redirect to
+accounts.youtube.com and google.de to set some cookies. Those are blocked (unless you visited one of those sites before in the same tab).
 
 Seems to work in the sense that it doesn't seem to break too many sites.
--- background.js
+++ background.js
@@ -1,14 +1,15 @@
 var strippedSendHeaders = {
-    "User-Agent": true,
-    "Referer": true,
-    "Cookie": true,
+    "user-agent": true,
+    "referer": true,
+    "cookie": true,
     // These leak too much info:
-    "Accept": true,
-    "Accept-Language": true,
+    "accept": true,
+    "accept-language": true,
 };
 
+// Headers must be in lowercase
 var strippedReceivedHeaders = {
-    "Set-Cookie": true,
+    "set-cookie": true,
     // More?
 };
 
@@ -23,6 +24,10 @@ var staticWhitelist = {
 // This gets updated while you browse. Keys are [tabid, hostname].
 var dynamicWhitelist = {};
 
+// Unknown hosts we redirect to get put on this list. When they redirect as the
+// first thing they do we don't put them on the dynamic whitelist.
+var cautiousList = {};
+
 function getHostname(url) {
     if (match = url.match(/^https?:\/\/([^\/]+)\//)) {
         return match[1];
@@ -30,7 +35,7 @@ function getHostname(url) {
 }
 
 function onBeforeHeadersSend(details) {
-    // console.log("onBeforeHeadersSend", details.url, details.type, details.tabId);
+    // console.log("onBeforeHeadersSend", details.url);
     var requestHost = '';
     var hostname = getHostname(details.url);
     if (! hostname) {
@@ -45,22 +50,26 @@ function onBeforeHeadersSend(details) {
     if (details.type === "main_frame") {
         // Request is top level. We accept this host from now on for this tab.
         // console.log("Accepting due to main_frame: ", hostname);
-        if (! ([details.tabId, hostname] in dynamicWhitelist)) {
-            console.log("Accepting due to main_frame: " + hostname + ", tab " + details.tabId);
+        if (hostname in cautiousList) {
+            console.log("not setting whitelist due to cautiousList", hostname);
+        } else {
+            if (! ([details.tabId, hostname] in dynamicWhitelist)) {
+                console.log("Accepting due to main_frame: " + hostname + ", tab " + details.tabId);
+            }
+            dynamicWhitelist[[details.tabId, hostname]] = true;
         }
-        dynamicWhitelist[[details.tabId, hostname]] = true;
-        return;
+        return
     }
     if ([details.tabId, hostname] in dynamicWhitelist) {
         // Known host. Allow all headers.
         // console.log("Allow as-is", details.url);
-        return;
+        return
     }
 
     // 3rd party request. Strip some headers.
     var changed = false;
     for (var i = 0; i < details.requestHeaders.length; ) {
-        if (details.requestHeaders[i].name in strippedSendHeaders) {
+        if (details.requestHeaders[i].name.toLowerCase() in strippedSendHeaders) {
             // console.log("Stripping send header ", details.requestHeaders[i], details.url);
             details.requestHeaders.splice(i, 1);
             changed = true;
@@ -76,7 +85,7 @@ function onBeforeHeadersSend(details) {
 
 function onBeforeHeadersResponse(details) {
     // console.log("onBeforeHeadersResponse", details.url);
-    var to, host;
+    var to, host, stripHeaders;
 
     host = getHostname(details.url);
     if (! host) {
@@ -87,17 +96,62 @@ function onBeforeHeadersResponse(details) {
         return;
     }
 
-    if (host in dynamicWhitelist) {
-        // Known host. Allow all headers.
+    stripHeaders = true;
+    if ([details.tabId, host] in dynamicWhitelist) {
+        console.log("response, host in dyn whitelist: ", host);
+        stripHeaders = false;
+        // Known host. Allow all headers
         // console.log("Allow received as-is", details.url);
+        //
+        // Check for redirects to new hosts. We don't accept those that easily.
+        for (var i = 0; i < details.responseHeaders.length; i++) {
+            // console.log("Check header ", details.responseHeaders[i].name);
+            if (details.responseHeaders[i].name.toLowerCase() === 'location') {
+                to = getHostname(details.responseHeaders[i].value);
+                if (!(to in dynamicWhitelist)) {
+                    console.log("Setting on cautiousList: ", to);
+                    cautiousList[to] = true
+                }
+            }
+        }
+    }
+
+    if (host in cautiousList) {
+        console.log("in cautiousList: ", host);
+        // This is a new host we just redirected to. If it redirects again
+        // ignore it, otherwise it can go on the dynamic whitelist.
+        var redirecting = false;
+        for (var i = 0; i < details.responseHeaders.length; i++) {
+            // console.log("Check header ", details.responseHeaders[i].name);
+            if (details.responseHeaders[i].name.toLowerCase() === 'location') {
+                console.log("Redirecting again!");
+                redirecting = true;
+                // Taint the next hop as well, if needed.
+                to = getHostname(details.responseHeaders[i].value);
+                if (!(to in dynamicWhitelist)) {
+                    console.log("Setting on cautiousList: ", to);
+                    cautiousList[to] = true
+                }
+                break;
+            }
+        }
+        if (! redirecting) {
+            console.log("Accepting " + host);
+            delete cautiousList[host];
+            dynamicWhitelist[[details.tabId, host]] = true;
+            stripHeaders = false;
+        }
+    }
+    if (! stripHeaders) {
+        console.log("Accepting headers for ", host);
         return;
     }
 
-    // 3rd party request. Strip some headers.
+    // 3rd party request. Strip some incoming headers.
     var changed = false;
     for (var i = 0; i < details.responseHeaders.length; ) {
-        if (details.responseHeaders[i].name in strippedReceivedHeaders) {
-            // console.log("Stripping received header ", details.responseHeaders[i], details.url);
+        if (details.responseHeaders[i].name.toLowerCase() in strippedReceivedHeaders) {
+            console.log("Stripping received header ", details.responseHeaders[i].name, details.responseHeaders[i].value, details.url);
             details.responseHeaders.splice(i, 1);
             changed = true;
             continue;
